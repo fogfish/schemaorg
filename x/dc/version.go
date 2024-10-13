c76b8001f1e96377d561e5aef9c5bf526f658cce
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-10-13 13:11:18 +0300

    dublin core metadata

---
+++ x/dc/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2023 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/schemaorg
+//
+
+package dc
+
+const Vsn = "x/dc/v0.1.0"
