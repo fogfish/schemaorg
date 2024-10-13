dec2e428f012a8ce42a865ab9945a308f9912e7d
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-10-13 13:14:26 +0300

    fix copyrights

--- x/dc/types.go
+++ x/dc/types.go
@@ -1,11 +1,3 @@
-//
-// Copyright (C) 2023 - 2024 Dmitry Kolesnikov
-//
-// This file may be modified and distributed under the terms
-// of the MIT license.  See the LICENSE file for details.
-// https://github.com/fogfish/schemaorg
-//
-
 package dc
 
 import "time"
